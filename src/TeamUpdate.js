86b5a27ca7e01886492ffbb356cdeb7dc2cc4707
nathantbissell <nathantbissell@gmail.com>
2019-01-23 21:19:53 -0500

    added fields for teamCreate

--- src/TeamUpdate.js
+++ src/TeamUpdate.js
@@ -1,33 +1,122 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import apiUrl from './apiConfig'
 
 class TeamUpdate extends Component {
   // this is your basic constructor setup
   constructor(props) {
     super(props)
     this.state = {
-      
+      id: '',
+      qbName: '',
+      qbDollar: '',
+      rbName: '',
+      rbDollar: '',
+      wrName: '',
+      wrDollar: '',
+      teName: '',
+      teDollar: '',
+      dstName: '',
+      dstDollar: '',
+      message: null
     }
-    this.updateTeam = this.updateTeam.bind(this)
   }
 
-  updateTeam = () => {
-    axios
-      .patch('http://localhost:4741/team/:id', {
-        team: {
-          
-        }
-      })
-      .then(res =>
-        this.setState({
-          message: `made a new team with ID: ${res.data.team.id}`
-        })
-      )
-      .catch(this.setState({ message: 'You have invalid form data' }))
-  }
+  // updateTeam = event => {
+  //   event.preventDefault()
+  //   const url = apiUrl + `/teams/${id}`
+  //   axios
+  //     .patch('http://localhost:4741/team/:id', {
+  //       team: {
+
+  //       }
+  //     })
+  //     .then(res =>
+  //       this.setState({
+  //         message: `made a new team with ID: ${res.data.team.id}`
+  //       })
+  //     )
+  //     .catch(this.setState({ message: 'You have invalid form data' }))
+  // }
+
+  onIdChange = event => this.setState({ _id: event.target.value })
+
+  onQBNameChange = event => this.setState({ qbName: event.target.value })
+  onQBDollarChange = event => this.setState({ qbDollar: event.target.value })
+
+  onRBNameChange = event => this.setState({ rbName: event.target.value })
+  onRBDollarChange = event => this.setState({ rbDollar: event.target.value })
+
+  onWRNameChange = event => this.setState({ wrName: event.target.value })
+  onWRDollarChange = event => this.setState({ wrDollar: event.target.value })
+
+  onTENameChange = event => this.setState({ teName: event.target.value })
+  onTEDollarChange = event => this.setState({ teDollar: event.target.value })
+
+  onDSTNameChange = event => this.setState({ dstName: event.target.value })
+  onDSTDollarChange = event => this.setState({ dstDollar: event.target.value })
+
   render() {
     return (
-      <div>
+      <div className='playerAddMain'>
+        <form onSubmit={this.updateTeam}>
+          <input
+            placeholder='QB Name'
+            value={this.state.qbName}
+            onChange={this.onQBNameChange}
+          />
+          <input
+            placeholder='QB Dollars'
+            value={this.state.qbDollar}
+            onChange={this.onQBDollarChange}
+          />
+          
+          <input
+            placeholder='RB Name'
+            value={this.state.rbName}
+            onChange={this.onRBNameChange}
+          />
+          <input
+            placeholder='RB Dollars'
+            value={this.state.rbDollar}
+            onChange={this.onRBDollarChange}
+          />
+
+          <input
+            placeholder='WR Name'
+            value={this.state.wrName}
+            onChange={this.onWRNameChange}
+          />
+          <input
+            placeholder='WR Dollars'
+            value={this.state.wrDollar}
+            onChange={this.onWRDollarChange}
+          />
+
+          <input
+            placeholder='TE Name'
+            value={this.state.teName}
+            onChange={this.onTENameChange}
+          />
+          <input
+            placeholder='TE Dollars'
+            value={this.state.teDollar}
+            onChange={this.onTEDollarChange}
+          />
+
+          <input
+            placeholder='DST Name'
+            value={this.state.dstName}
+            onChange={this.onDSTNameChange}
+          />
+          <input
+            placeholder='DST Dollars'
+            value={this.state.dstDollar}
+            onChange={this.onDSTDollarChange}
+          />
+          <input type='submit' value='Create Team!' />
+        </form>
+        {this.state.message && <span> {this.state.message}</span>}
       </div>
     )
   }
